a4f9769b9f88e961f94ee17bf2163810633c9cc9
Paul Mekhail <paul.mekhail4@gmail.com>
2022-04-12 14:08:24 +0200

    presque fini

--- src/lib/block.c
+++ src/lib/block.c
@@ -1,4 +1,5 @@
 #include "headers/block.h"
+#include <stdlib.h>
 
 void ecrire_block(Block *b, char *nomFic){
     FILE *fic = fopen(nomFic, "w");
@@ -26,7 +27,7 @@ Block *lire_block(char *nomFic){
     Block *b = (Block*)malloc(sizeof(Block));
     char buffer[256];
 
-    char *keyStr;
+    char *keyStr = NULL;
     fgets(buffer, 256, fic);
     if(sscanf(buffer, "%s\n", keyStr) == 1){
         b->author = str_to_key(keyStr);
@@ -37,7 +38,7 @@ Block *lire_block(char *nomFic){
         return NULL;
     }
 
-    char *hash;
+    unsigned char *hash = NULL;
     fgets(buffer, 256, fic);
     if(sscanf(buffer, "%s\n", hash) == 1){
         b->hash = hash;
@@ -47,7 +48,7 @@ Block *lire_block(char *nomFic){
         return NULL;
     }
 
-    char *prevHash;
+    unsigned char *prevHash = NULL;
     fgets(buffer, 256, fic);
     if(sscanf(buffer, "%s\n", prevHash) == 1){
         b->previous_hash = prevHash;
@@ -59,7 +60,7 @@ Block *lire_block(char *nomFic){
 
     int nonce;
     fgets(buffer, 256, fic);
-    if(sscanf(buffer, "%d\n", nonce) == 1){
+    if(sscanf(buffer, "%d\n", &nonce) == 1){
         b->nonce = nonce;
     }
     else{
@@ -86,7 +87,85 @@ Block *lire_block(char *nomFic){
 }
 
 char *block_to_str(Block *block){
-    char *c = NULL;
+    char *key = key_to_str(block->author);
+    unsigned char *previous_hashchar = block->previous_hash;
+    int lenNonceChar;
+    for(int i =0 ;((block->nonce)/(pow(10,i)))>=1;i++){
+        lenNonceChar+=1;
+    }
+    char *noncechar = malloc(lenNonceChar*(sizeof(char)));
+    sprintf(noncechar, "%d", block->nonce);
+    CellProtected *tmp = block->votes;
+    int lenVotes;
+    while(tmp->next==NULL){
+        char *tmp2 = protected_to_str(tmp->data);
+        lenVotes+=strlen(tmp2);
+        tmp=tmp->next;
+        free(tmp2);
+    }
+    int lenTotale=lenVotes+lenNonceChar+strlen(previous_hashchar);
+    char *res = malloc(lenTotale+1*(sizeof(char)));
 
-    return c;
+    strcat(res,key);
+    
+    strcat(res,previous_hashchar);
+    
+    tmp=block->votes;
+    while(tmp->next==NULL){
+        char *tmp2 = protected_to_str(tmp->data);
+        strcat(res,tmp2);
+        tmp=tmp->next;
+        free(tmp2);
+
+    }
+    
+    strcat(res,noncechar);
+	return res;
+}
+
+unsigned char *miaou256(const char *mess){
+	unsigned char *res = SHA256(mess, strlen(mess), 0);
+	return res;
+}
+
+void compute_proof_of_work(Block* B,int d){
+    B->nonce=0;
+    unsigned char* testHexa=miaou256(block_to_str(B));
+    int i=1;
+    while (i){
+        int p=0;
+        for(int j=0;j<d;j++){
+            if (testHexa[j]!=0){
+                p-=1;
+                B->nonce+=1;
+                testHexa=miaou256(block_to_str(B));
+                break;
+            }
+            else{
+                p+=1;
+            }
+		}
+        if (p==d){
+            i=0;
+        }
+    }
+}
+							 
+int verify_block(Block* b, int d){
+    unsigned char* testHexa=miaou256(block_to_str(b));
+    int p=0;
+    for(int j=0;j<d;j++){
+        if (testHexa[j]!=0){
+            p-=1;
+        }
+        else{
+            p+=1;
+        }
+	}
+    if (p==d){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
--- src/lib/hashCell.c
+++ src/lib/hashCell.c
@@ -9,7 +9,8 @@ HashCell *create_hashcell(Key *key){
         printf("erreur d'allocation\n");
         return NULL;
     }
-    h->key = key;
+    h->key = malloc(sizeof(Key));
+	init_key(h->key, key->val, key->n);
     h->val = 0;
     return h;
 }
@@ -18,14 +19,28 @@ int hash_function(Key *key, int size){
     //paramètres: clé de type Key et la taille de la table
     //
     //valeur de retour: la valeur de hachage de key->val
-	return (int)(size*((key->val)*((sqrt(5)-1)/2)-(int)((key->val)*((sqrt(5)-1)/2))));
+	if(!key){
+		printf("Key non initialisé\n");
+		return -1;	
+	}
+	float nbo = (sqrtf(5)-1)/2; //on utilise le nombre d'or diminué de 1
+    return ((int) floor(size * (key->val * nbo - floor(key->val * nbo))));
+	//return (int)(size*((key->val)*((sqrt(5)-1)/2)-(int)((key->val)*((sqrt(5)-1)/2))));
 }
 
 int find_position(HashTable *t, Key *key){
     //paramètres: une table de hashage t clé de type Key
-    //
-    //valeur de retour: l'indice 
+    //renvoie l'indice de la clé dans la table sinon l'indice ou elle devrait se trouver
+    //valeur de retour: l'indice de la clé dans la table
+	if(!key || !t){
+		printf("erreur\n");
+		return -1;
+	}
     int cle = hash_function(key, t->size);
+	if(!t->tab){
+		printf("tableau non initialisé\n");
+		return 0;	
+	}
     if(t->tab[cle] != NULL){
         if(t->tab[cle]->key){
             if(t->tab[cle]->key->val == key->val && t->tab[cle]->key->n == key->n){
@@ -33,43 +48,58 @@ int find_position(HashTable *t, Key *key){
             }
             else{
                 int i = 1;
-                while(t->tab[(cle+i)%t->size] && i%t->size != 0){
+				int k = (cle+i)%t->size;
+                while(t->tab[k] && i%t->size){
                     if(t->tab[cle]->key->val == key->val && t->tab[cle]->key->n == key->n){
-                        return (cle+i)%t->size;
+                        return k;
                     }
                     i++;
+					k = (cle+i)%t->size;
                 }
-                if(i%t->size == 0) return -1;
-                return (cle+i)%t->size;
+                if(i%t->size == 0) return cle;
+                return k;
             }
         }
     }
-    return cle;
+	return cle;
 }
 
 HashTable *create_hashtable(CellKey *keys, int size){
     //paramètres: une liste chaînée de clé et la taille de la liste
     //
     //valeur de retour: la table de hachage crée
-    HashTable *h = (HashTable*)malloc(sizeof(HashTable)); //Allocation de la mémoire
+    HashTable *h = (HashTable*)calloc(1, sizeof(HashTable)); //Allocation de la mémoire
     if(!h){
         printf("erreur d'allocation\n");
         return NULL;
+	}
+	
+    h->tab = (HashCell**)calloc(size, sizeof(HashCell*)); //Allocation de la mémoire
+	if(!h->tab){
+		printf("Erreur d'allocation\n");
+		free(h);
+		return NULL;
+	}
+	
+	h->size = size;
+	
+	if (len_cellkey(keys) > size)
+    {
+        puts("Attention, le tableau de hashage n'est pas assez grand pour toutes les clés.");
     }
-    h->tab = (HashCell**)malloc(sizeof(HashCell*)*size); //Allocation de la mémoire
-    if(!(h->tab)){
-        printf("erreur d'allocation\n");
-        free(h);
-        return NULL;
-    }
-    h->size = size;
+
     CellKey *tmp = keys;
-    for(int i=0; i<h->size; i++){
-        h->tab[i] = NULL;
-    }
+	int i = 0;
+	
     while(tmp){
-        h->tab[find_position(h, tmp->data)] = create_hashcell(tmp->data);
-        tmp = tmp->next;
+		i = find_position(h, tmp->data);
+		if(!(h->tab[i])){
+        	h->tab[i] = create_hashcell(tmp->data);
+		}
+		else{
+			printf("le tableau est trop petit\n");
+		}
+		tmp = tmp->next;
     }
     return h;
 }
@@ -78,15 +108,23 @@ void delete_hashtable(HashTable* t){
     //paramètres: une table de hachage t
     //
     //valeur de retour: aucune
+	if(!t){
+		printf("hash table inexistante \n");
+		return;
+	}
     for(int i=0; i<t->size; i++){
-        free(t->tab[i]->key);
-        free(t->tab[i]);
+		if(t->tab[i]){
+			if(t->tab[i]->key){
+				free(t->tab[i]->key);	
+			}
+			free(t->tab[i]);
+		}
     }
     free(t->tab);
     free(t);
 }
 
-Key* compute_winner(CellProtected* decl, CellKey* candidates, CellKey* voters, int sizeC, int sizeV){
+Key* compute_winner(CellProtected* decl, CellKey* candidates, CellKey* voters, int sizeV, int sizeC){
     //paramètres: 
     //
     //valeur de retour: la clé du gagnat
@@ -123,6 +161,13 @@ Key* compute_winner(CellProtected* decl, CellKey* candidates, CellKey* voters, i
             }
         }
     }
-    Key *winner = hc->tab[find_position(hc, max->key)]->key;
+    Key *winner = (Key*)malloc(sizeof(Key));
+	if(!winner){
+		printf("erreur d'allocation\n");
+		return NULL;
+	}
+	init_key(winner, hc->tab[find_position(hc, max->key)]->key->val, hc->tab[find_position(hc, max->key)]->key->n) ;
+	delete_hashtable(hc);
+	delete_hashtable(hv);
     return winner;
 }
