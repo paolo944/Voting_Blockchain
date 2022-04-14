5689079981b9a4a468bc767db072e4079cb278ed
PaulinDeuxTrois <95508677+PaulinDeuxTrois@users.noreply.github.com>
2022-04-14 18:09:52 +0200

    9 mieux fait (par paul le bg)

--- src/lib/vote.c
+++ src/lib/vote.c
@@ -5,9 +5,15 @@
 
 
 void submit_vote(Protected* p){
-    CellProtected* vote=malloc(sizeof(CellProtected));
-    vote->data=p;
-    vote->next=NULL;
+    FILE *F=foppen("Pending_votes.txt","a");
+    char* pstr=protected_to_str(p);
+    fprintf(F,"%s",pstr);
+    fputc(F,'\n');
+    fclose(F);
+    free(pstr);    
+}
+/*
+CellProtected* vote=create_cell_protected(p);
     Block* bloc = malloc(sizeof(Block));
     bloc->author=p->pKey;
     bloc->previous_hash=NULL;
@@ -16,5 +22,4 @@ void submit_vote(Protected* p){
     bloc->votes=vote;                           //compute_proof_of_work mettra tout seul la bonne valeur hash
     compute_proof_of_work(bloc,5);
     ecrire_block(bloc,"Pending_votes.txt");
-    
-}
+*/
