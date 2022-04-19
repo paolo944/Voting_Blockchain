356e52e8985bdf71cdd8834d109e42f5543430f4
PaulinDeuxTrois <95508677+PaulinDeuxTrois@users.noreply.github.com>
2022-04-19 10:55:53 +0200

    devbut exo 9

---
+++ src/lib/headers/vote.h
@@ -0,0 +1,7 @@
+#include "protected.h"
+#include "cellProtected.h"
+#include "block.h"
+#include "cellTree.h"
+
+void submit_vote(Protected* p);
+void create_block(CellTree* tree, Key* author, int d);
--- src/lib/vote.c
+++ src/lib/vote.c
@@ -1,7 +1,4 @@
-#include "protected.h"
-#include "cellProtected.h"
-#include "block.h"
-#include "cellTree.h"
+#include "vote.h"
 
 
 void submit_vote(Protected* p){
@@ -23,3 +20,9 @@ CellProtected* vote=create_cell_protected(p);
     compute_proof_of_work(bloc,5);
     ecrire_block(bloc,"Pending_votes.txt");
 */
+
+void create_block(CellTree* tree, Key* author, int d) {
+    Block* Pending_block=lire_block("Pending_vote.txt");
+    Pending_block->author=author;
+    Pending_block->nonce=d;
+}
